9eba2199682b37bbbd95553c2f0c5abf50df9c5e
Samuel James <bsbcraftplays@gmail.com>
2020-09-29 11:45:18 -0300

    Correção de exceções na implementação de Domínio Classe

--- trabalho1/Dominios/Classe.cpp
+++ trabalho1/Dominios/Classe.cpp
@@ -1,8 +1,9 @@
+#include "Classe.h"
 
 /**
  * Implementação de construtor da classe Classe
  */
-Classe::Classe(string valor) {
+Classe::Classe(std::string valor) {
 	Classe::validate(valor);
 	this->valor = valor;
 }
@@ -10,14 +11,14 @@ Classe::Classe(string valor) {
 /**
  * TODO: Explicar como funciona a validação da classe
  */
-Classe::validate(string valor) throw invalid_argument {
+void Classe::validate(std::string valor) throw (std::invalid_argument) {
 	// TODO: Implementar função de validação
 }
 
 /**
  * Implementação de método setter da classe Classe
  */
-void Classe::setValor(string valor) {
+void Classe::setValor(std::string valor) {
 	Classe::validate(valor);
 	this->valor = valor;
 }
