5dcd0d5f57a37ccdff80b3f47d425e7b3a9514e5
Samuel James <bsbcraftplays@gmail.com>
2020-10-22 21:47:37 -0300

    Codificação de exemplo: Entidade Aplicação

---
+++ Entidades/Aplicacao.cpp
@@ -0,0 +1,13 @@
+#include "Aplicacao.h"
+
+void Aplicacao::setCodigo(CodigoDeAplicacao codigo) {
+    this->codigo = codigo;
+}
+
+void Aplicacao::setValor(ValorDeAplicacao valor) {
+    this->valor = valor;
+}
+
+void Aplicacao::setData(Data data) {
+    this->data = data;
+}
---
+++ Entidades/Aplicacao.h
@@ -0,0 +1,41 @@
+#ifndef APLICACAO_H_INCLUDED
+#define APLICACAO_H_INCLUDED
+
+#include "../Dominios/CodigoDeAplicacao.h"
+#include "../Dominios/ValorDeAplicacao.h"
+#include "../Dominios/Data.h"
+
+#include <string>
+
+class Aplicacao {
+
+    private:
+		CodigoDeAplicacao   codigo;
+        ValorDeAplicacao    valor;
+        Data                data;
+
+    public:
+        Aplicacao(CodigoDeAplicacao codigo, ValorDeAplicacao valor, Data data) : codigo(codigo), valor(valor), data(data) { }
+        
+        Aplicacao(std::string codigo, double valor, std::string data) : Aplicacao(CodigoDeAplicacao(codigo), ValorDeAplicacao(valor), Data(data)){ }
+
+        CodigoDeAplicacao getCodigo() {
+            return this->codigo;
+        }
+
+        ValorDeAplicacao getValor() {
+            return this->valor;
+        }
+
+        Data getData() {
+            return this->data;
+        }
+
+        void setCodigo(CodigoDeAplicacao codigo);
+
+        void setValor(ValorDeAplicacao valor);
+
+        void setData(Data data);
+};
+
+#endif // APLICACAO_H_INCLUDED
