c87a75fe407c00c6b1ba31ea1ef7f6c0ad4f6b14
Samuel James <bsbcraftplays@gmail.com>
2020-12-03 11:20:29 -0300

    Iniciando implementação do gerenciador de produtos

---
+++ Servicos/Controladoras/MeuGerenciadorDeProduto.cpp
@@ -0,0 +1,38 @@
+#include "MeuGerenciadorDeProduto.h"
+
+MeuGerenciadorDeProduto* MeuGerenciadorDeProduto::instance = 0;
+
+MeuGerenciadorDeProduto* MeuGerenciadorDeProduto::getInstance() {
+    if (instance == nullptr)
+        instance = new MeuGerenciadorDeProduto();
+    return instance;
+}
+
+std::list<Classe> MeuGerenciadorDeProduto::listarClasses() {
+    std::list<Classe> lista;
+    return lista;
+}
+
+Produto* MeuGerenciadorDeProduto::listarProduto(const Classe&classe) {
+    return NULL;
+}
+
+bool MeuGerenciadorDeProduto::cadastrarProduto(const Produto&produto) {
+    std::string query = "INSERT INTO PRODUTOS(CODIGO_DE_PRODUTO, CLASSE, EMISSOR, PRAZO, DATA, TAXA, HORARIO, VALOR_MINIMO)";
+    query += "VALUES(";
+    query += "'" + produto.getCodigo().getValor() + "', "; 
+    query += "'" + produto.getClasse().getValor() + "', ";
+    query += "'" + produto.getEmissor().getValor() + "', "; 
+    query += "'" + std::to_string(produto.getPrazo().getValor()) + "', ";
+    query += "'" + produto.getVecimento().getValor() + "', ";
+    query += "'" + std::to_string(produto.getTaxa().getValor()) + "', "; 
+    query += "'" + produto.getHorario().getValor() + "', "; 
+    query += "'" + std::to_string(produto.getValorMinimo().getValor()) + "'";
+    query += ")";
+    ResultadoSQL* resultado = GerenciadorBancoSQL::getInstance()->executar(query);
+    return (resultado != NULL && resultado->sucesso);
+}
+
+bool MeuGerenciadorDeProduto::descadastrar(const CodigoDeProduto&codigo) {
+    return false;
+}
--- Servicos/Controladoras/MeuGerenciadorDeProduto.h
+++ Servicos/Controladoras/MeuGerenciadorDeProduto.h
@@ -3,7 +3,18 @@
 #include "../BancoSQL/GerenciadorBancoSQL.h"
 #include "../Interfaces/IGerenciadorDeProduto.h"
 
+class MeuGerenciadorDeProduto: public IGerenciadorDeProduto {
+    private:
+        static MeuGerenciadorDeProduto* instance;
+        MeuGerenciadorDeProduto(){};
 
+    public:
+        static MeuGerenciadorDeProduto* getInstance();
+        std::list<Classe> listarClasses();
+        Produto* listarProduto(const Classe&classe);
+        bool cadastrarProduto(const Produto&produto);
+        bool descadastrar(const CodigoDeProduto&codigo);
+};
 
 
 #endif//MEUGERENCIADORDEPRODUTO_H_INCLUDED
